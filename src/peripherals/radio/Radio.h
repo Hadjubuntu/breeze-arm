b9791c0143140c96054cb7bcd8e652a98f8b90db
Adrien <adrien.hadj.salah@gmail.com>
2015-09-13 16:24:02 +0200

    New arm code

---
+++ src/peripherals/radio/Radio.h
@@ -0,0 +1,20 @@
+/*
+ * Radio.h
+ *
+ *  Created on: Jul 14, 2015
+ *      Author: adrien
+ */
+
+#ifndef PERIPHERALS_RADIO_RADIO_H_
+#define PERIPHERALS_RADIO_RADIO_H_
+
+
+class Radio {
+public:
+	void check() {
+
+	}
+};
+
+
+#endif /* PERIPHERALS_RADIO_RADIO_H_ */
